07530dc43438127b888a9a13d9211ca59285be3d
amitgeek435 <amitgeek435@gmail.com>
2022-06-21 18:01:48 +0530

    slot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Container } from "react-bootstrap";
+// import Form from "react-bootstrap/Form";
+
+const Slotm = () => {
+  let x = ":happy";
+  let y = ":happy";
+  let z = ":happy";
+  if (x === y && y === z) {
+    return (
+      <>
+        <div>
+          <h2 className="h1-title">
+            {x}
+            {y}
+            {z}
+          </h2>
+          <p className="border border-secondary text-success text-center bg-warning py-3 rounded-circle">
+            This Is Matching
+          </p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h2 className="h1-title">
+            {x}
+            {y}
+            {z}
+          </h2>
+          <p className="border border-secondary text-primary text-center bg-warning py-3 rounded-circle">
+            This Is Not Matching
+          </p>
+        </div>
+      </>
+    );
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <Container>
+        <h1 className="h1-title">Hello Slot Machine</h1>
+        <p className="border border-secondary text-danger text-center bg-warning py-3 rounded-circle">
+          Slot Machine Game
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="card w-50">
+          <Slotm />
+        </div>
+      </Container>
+    </>
   );
 }
 
